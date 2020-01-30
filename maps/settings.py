29e65709175dc40d2a91f81d410c078a7372255d
bed72 <gjramos100@gmail.com>
2020-01-30 17:24:32 -0300

    Algumas pequenas alterações noarquivo settings.py!

--- maps/settings.py
+++ maps/settings.py
@@ -1,18 +1,16 @@
 import os
 
+# Para indetificar a raiz do modulo python
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Database
-DATABASES = {
+""" Futura conexão com DATABASE """
+DATABASES: dict = {
     "host": "localhost",
     "port": "5432",
     "user": "postgres",
     "pasword": "cicada3301",
 }
 
-
-STATICFILES_DIRS = [
+# Lista que indentifa os arquivos .osm
+STATICFILES_DIRS: list = [
     os.path.join(os.path.normpath(BASE_DIR), "maps\\files\\"),
 ]
-
